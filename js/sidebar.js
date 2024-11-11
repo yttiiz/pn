93a5e57c5c2efc93f9ec142aa636a8bb633ae786
yttiiz <dominique.talis@proton.me>
2024-11-10 20:51:13 -0400

    feat(did-website): implement sidebar data

---
+++ js/sidebar.js
@@ -0,0 +1,57 @@
+// Elements
+const sidebar = document.querySelector(".sidebar");
+const data = await fetch("./data/data.json")
+	.then((data) => data.json())
+	.then((data) => data);
+
+// Hydratation
+Object.keys(data).map((key) => {
+	for (const titleItem of data[key]) {
+		const mainItem = document.createElement("ul");
+		const mainLink = document.createElement("a");
+
+		if (titleItem.subtitles.length) {
+			mainLink.textContent = titleItem.title;
+			mainLink.href = titleItem.href;
+			mainItem.appendChild(mainLink);
+
+			for (const subtitleKey of Object.keys(titleItem.subtitles[0])) {
+				const subtitleItem = titleItem.subtitles[0][subtitleKey];
+				const secondaryList = document.createElement("li");
+
+				if (subtitleItem.content.length) {
+					const item = document.createElement("ul");
+					const link = document.createElement("a");
+
+					link.textContent = subtitleItem.subtitle;
+					link.href = subtitleItem.href;
+
+					item.appendChild(link);
+					secondaryList.appendChild(item);
+
+					for (const lastItem of subtitleItem.content) {
+						const tercaryItem = document.createElement("li");
+						const tercaryLink = document.createElement("a");
+
+						tercaryLink.textContent = lastItem.item;
+						tercaryLink.href = lastItem.href;
+
+						tercaryItem.appendChild(tercaryLink);
+						item.appendChild(tercaryItem);
+					}
+
+					mainItem.appendChild(secondaryList);
+				} else {
+					//
+				}
+			}
+		} else {
+			mainLink.href = titleItem.href;
+			mainLink.textContent = titleItem.title;
+
+			mainItem.appendChild(mainLink);
+		}
+
+		sidebar.appendChild(mainItem);
+	}
+});
---
+++ main.js
@@ -0,0 +1,2 @@
+import "./js/sidebar.js";
+
