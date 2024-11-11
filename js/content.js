f5d0772de3e6e119fad2f5021f83b784a0a561af
yttiiz <dominique.talis@proton.me>
2024-11-11 11:21:30 -0400

    feat(did-website): hydrate sections

--- js/content.js
+++ js/content.js
@@ -1,7 +1,68 @@
-import { createElement } from "./utils.js";
+import { createElement, insertElement } from "./utils.js";
 
 // Elements
 const container = document.querySelector(".sections");
 const section = createElement("section");
+const data = await fetch("./data/main.json")
+	.then((data) => data.json())
+	.then((data) => data);
+
+// Hydratation
+Object.keys(data).map((key) => {
+	const { title, subtitles, id } = data[key];
+	const sectionContainer = createElement("div");
+	const titleElement = createElement("h2");
+
+	insertElement({
+		element: titleElement,
+		textContent: title,
+		id,
+		parent: sectionContainer,
+	});
+	section.appendChild(sectionContainer);
+
+	if (subtitles) {
+		for (const subtitleObject of subtitles) {
+			const { id: subtitleId, subtitle, items, textContent } = subtitleObject;
+			const subtitleElement = createElement("h3");
+
+			insertElement({
+				element: subtitleElement,
+				textContent: subtitle,
+				id: subtitleId,
+				parent: sectionContainer,
+			});
+
+			if (textContent) {
+				insertElement({
+					element: createElement("p"),
+					textContent,
+					parent: sectionContainer,
+				});
+			}
+
+			if (items) {
+				for (const itemObject of items) {
+          const { item, id: itemId, textContent: itemTextContent } = itemObject;
+          const itemElement = createElement("h4");
+          const paragraphElement = createElement("p");
+
+          insertElement({
+            element: itemElement,
+            id: itemId,
+            textContent: item,
+            parent: sectionContainer,
+          });
+
+          insertElement({
+            element: paragraphElement,
+            textContent: itemTextContent,
+            parent: sectionContainer,
+          });
+				}
+			}
+		}
+	}
+});
 
 container.appendChild(section);
--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,20 @@
  * @param {HTMLElementTagNameMap} selector
  */
 export const createElement = (selector) => document.createElement(selector);
+
+/**
+ * @param {{
+ * element: HTMLElement;
+ * textContent: string;
+ * id: string;
+ * parent: HTMLElement;
+ * }} 
+ */
+export const insertElement = ({ element, textContent, id, parent }) => {
+  element.textContent = textContent;
+  if (id) {
+    element.id = id;
+  }
+
+  parent.appendChild(element);
+}
